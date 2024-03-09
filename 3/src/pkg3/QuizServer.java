b619cb996cfb42f286fb32dd6f835ab48b8eb795
“tovanthieu” <tovanthieu2002@gmail.com>
2024-03-09 13:39:41 +0700

    Code Lap Trinh Mang Quiz

---
+++ 3/src/pkg3/QuizServer.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg3;
+
+/**
+ *
+ * @author ASUS
+ */
+public class QuizServer {
+
+    public static final String[][] QUESTIONS = {
+        {"Tai sao con ca khong bao gio hoc gioi?", "A. Vi no bi ho", "B. Vi no thich boi", "C. Vi no luoi", "D. Vi no qua ngu", "C"},
+        {"Tai sao cuoi cung cua sach lai nguoc?", "A. De ban doc co cam giac bi dao", "B. De tao kho khan cho ke copy", "C. Vi loi ich thuc su chi co the duoc dat ra neu ban doc da doc xong sach", "D. De lam gi, cuoi dau cuoi!", "D"},
+        {"Tai sao nguoi ta lai deo dong ho tren tay phai?", "A. De nhin thay gio khi nhin dong ho", "B. De the hien su phong cach", "C. De deo nhe nhang hon", "D. Khong biet, chi thay moi nguoi deo vay", "A"},
+        {"Tai sao bong bong nuoc lai tron?", "A. Vi bong bong nuoc duoc tao thanh tu hinh tron", "B. Vi nuoc co tinh chat hinh tron", "C. Vi luc hap dan giua cac phan tu nuoc", "D. Vi neu no tron, se xau ho", "C"},
+        {"Tai sao con gian phai chay nhanh nhu chong chong?", "A. De chong chong khong kip bat no", "B. De chong chong khong kip dung lai", "C. De chong chong khong kip nhin thay no", "D. De chong chong khong kip nhan ra no dang o dau", "B"},
+        {"Tai sao ban co the thay mui nuoc hoa nhung khong the thay mui hoi cua minh?", "A. Vi mui nuoc hoa thom qua", "B. Vi mui hoi cua minh khong thom", "C. Vi mui nuoc hoa la chat long nen bay hoi nhanh hon", "D. Vi mui hoi cua minh se theo ban suot doi", "C"},
+        {"Tai sao lai co cau 'Duoi chan hoa sua'?", "A. Vi hoa sua hay noi o phia duoi cay", "B. Vi hoa sua roi o phia tren cay", "C. Vi nguoi ta hay di tren duoi chan hoa sua", "D. Khong biet, chi thay moi nguoi noi vay", "A"},
+        {"Tai sao khong gian lai den trong khi mua he?", "A. Vi nang mat troi qua at", "B. Vi cac tia tu nang mat troi bi chen lan", "C. Vi mua he hay mua den", "D. Vi khong gian ghet mua he", "B"},
+        {"Tai sao cuon Qua Trinh Phat Trien Cua Con Ngua lai ngan gon?", "A. Vi con ngua phat trien nhanh", "B. Vi tac gia chi muon noi ve cai cuon sach ngan gon", "C. Vi con ngua khong co gi phat trien", "D. Vi nguoi doc khong muon doc dai dong", "D"},
+        {"Tai sao khi ta nghi ve mot cau hoi nhat dinh, co dau hieu la nao?", "A. Nao ta co dau hieu", "B. Nao ta co y tuong", "C. Nao ta co nghi ngo", "D. Nao ta dang lam viec", "B"}, // Them cac cau hoi va dap an khac tai day
+    };
+}
